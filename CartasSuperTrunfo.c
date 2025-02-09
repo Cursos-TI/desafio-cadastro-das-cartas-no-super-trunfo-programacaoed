76957566554eaecac6dd5ab08c7879a2c5b126e6
programacaoed <andersonsilvaer516@gmail.com>
2025-02-09 20:18:20 +0000

    alterações feitas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,29 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
-int main() {
+int main(){
+    int codigo, populacao;
+    char nome[50];
+    float area;
+
+    printf("Países\n");
+
+    printf("Código da cidade: \n");
+    scanf("%d", &codigo);
+
+    printf("Nome: \n");
+    scanf("%s", &nome);
+
+    printf("População: \n");
+    scanf("%d", &populacao);
+    
+    printf("Àrea: \n");
+    scanf("%f", &area);
+
+    printf("Código da cidade: %d - Nome: %s\n", codigo, nome);
+    printf("População: %d - Àrea: %f", populacao, area);
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
